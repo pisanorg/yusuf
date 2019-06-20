30ff2b09a858c557fe46f2ecae9ed39136da0d47
Yusuf Pisan <pisan@uw.edu>
2019-06-20 14:27:59 -0700

    Update simplecompile.sh

--- cpp/simplecompile.sh
+++ cpp/simplecompile.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# To more easily compile and run this program on CSS Linux Lab
-# Lines starting with '$' indicate what is typed on command line
+# simplecompile,sh: compile all cpp files as myprogram.exe and run some checks
 
 # if you get the following error:
 # -bash: ./simplecompile.sh: /bin/bash^M: bad interpreter: No such file or directory
-# run dos2unix to fix it
+# It means that the line-endings got changed between unix and Windows systems, to fix use:
 # $ dos2unix simplecompile.sh
 
 # make this file executable
@@ -20,7 +19,7 @@ echo "*** Compiling"
 g++ -std=c++11 -Wall -Wextra -Wno-sign-compare *.cpp -g -o myprogram.exe
 
 echo "*** cpplint"
-cpplint *.cpp *.hpp
+cpplint *.cpp
 
 echo "*** cppcheck"
 # cppcheck in CSS Linux Lab only knows about c++11
@@ -29,7 +28,7 @@ cppcheck --enable=all --force --inconclusive --language=c++ --std=c++11 --suppre
 
 echo "*** clang-tidy"
 # perform all possible checks
-# The "--" at the end is important. 
+# The "--" at the end is important. If cmake has created compile_commands.json file, it can be omitted
 clang-tidy -checks="*" *.cpp --
 
 echo "*** running"
